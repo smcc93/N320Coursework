03110b69eaacc402783bc42c7c6d1610cdffa6c6
Stewart <smccalle@iu.edu>
2019-10-03 22:12:00 -0400

    added code from video, completed back button and added data

--- labs/Week5/js/app.js
+++ labs/Week5/js/app.js
@@ -1,29 +1,55 @@
 Vue.component("student-card", {
   props: ["student", "isactive"],
   template:
-    "<div class='student' v-bind:class='{cardActive:isactive}'>{{student.name}} : {{student.skill}}</div>"
+    "<div class='student' v-bind:class='{ cardActive:isactive, cardOut:!isactive }'>{{ student.name }} : {{ student.skill }} <br> Joy: {{student.joy}}</div>"
 });
 
 var app = new Vue({
   el: "#app",
   data: {
     students: [
-      { name: "Sienna", skill: 2, joy: 0 },
+      { name: "Sienna", skill: 2, joy: 2 },
       { name: "Cyan", skill: 0, joy: 5 },
-      { name: "Magenta", skill: 3, joy: 3 }
+      { name: "Magenta", skill: 3, joy: 7 }
     ],
-    currentStudent: { name: "Sienna", skill: 2, joy: 0 },
-    currentStudentId: 0,
+    currentStudent: { name: "Sienna", skill: 2, joy: 2 },
+    curStudentId: 0,
     cardActive: true
   },
   methods: {
     arrowClicked: function() {
-      this.currentStudentId++;
-      this.currentStudent = this.students[this.currentStudentId];
-      if (this.currentStudentId >= this.students.length - 1) {
-        this.currentStudentId = -1;
-      }
       this.cardActive = !this.cardActive;
+
+      setTimeout(() => {
+        //modify the skill of the current student
+        //before moving onward:
+        this.currentStudent.skill++;
+        this.currentStudent.joy++;
+        //iteration code
+        this.curStudentId++;
+        this.currentStudent = this.students[this.curStudentId];
+
+        if (this.curStudentId >= this.students.length - 1) {
+          this.curStudentId = -1;
+        }
+
+        //animation trigger
+        this.cardActive = !this.cardActive;
+      }, 300);
+    },
+    backClick: function() {
+      this.cardActive = !this.cardActive;
+
+      setTimeout(() => {
+        this.currentStudent.joy--;
+        this.curStudentId--;
+        if (this.curStudentId < 0) {
+          this.curStudentId = 2;
+        }
+        this.currentStudent = this.students[this.curStudentId];
+
+        this.cardActive = !this.cardActive;
+      }, 300);
     }
   }
 });
