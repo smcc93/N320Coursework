ec50654a468c07e16fb9225c24abdf5f305dfe33
Stewart <smccalle@iu.edu>
2019-10-10 21:59:19 -0400

    finished win conditions and declaring winner

--- labs/Week6/js/app.js
+++ labs/Week6/js/app.js
@@ -3,6 +3,7 @@ var app = new Vue({
   data: {
     gameOver: false,
     playerTurn: 1,
+    winner: 0,
     grid: [
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
@@ -21,8 +22,8 @@ var app = new Vue({
         //copy grid to a temp var
         var tempGrid = this.grid.slice(0);
 
-        //modify cloned array
-        tempGrid[row][col] = this.playerTurn;
+        //modify the cloned version
+        tempGrid[moveRow][col] = this.playerTurn;
 
         //replace
         this.grid = tempGrid;
@@ -34,25 +35,71 @@ var app = new Vue({
         this.checkWin();
       }
     },
-    checkWin: function(row, col) {
-      //loop through all columns to check
-      for (i = 0; i <= this.grid[row].length; i++) {
-        for (j = 0; j <= this.grid[col].length; j++) {
-          //check for 4 in a row, column, or diagonal
-          if (this.grid[row][col] !== 0) {
-            if (this.grid[row][col]) {
-            }
-          }
-        }
-      }
-      //if win found set over to true
-    },
-
     lowestMove: function(col) {
       for (var row = 5; row >= 0; row--) {
         if (this.grid[row][col] == 0) {
           //if it is free, return row index
           return row;
+        }
+      }
+    },
+    checkWin: function(row, col) {
+      //loop through all columns to check
+      //if win found set over to true
+      for (var row = 0; row < this.grid.length; row++) {
+        for (var col = 0; col < this.grid[row].length; col++) {
+          if (row - 3 >= 0) {
+            if (
+              //check through row
+              this.grid[row - 3][col] == this.grid[row - 2][col] &&
+              this.grid[row - 1][col] == this.grid[row][col] &&
+              this.grid[row][col] == this.grid[row - 3][col] &&
+              (this.grid[row][col] == 1 || this.grid[row][col] == 2)
+            ) {
+              this.gameOver = true;
+              this.winner = this.playerTurn;
+              break;
+            }
+          }
+          if (col - 3 >= 0) {
+            if (
+              //check through columns
+              this.grid[row][col - 3] == this.grid[row][col - 2] &&
+              this.grid[row][col - 1] == this.grid[row][col] &&
+              this.grid[row][col] == this.grid[row][col - 3] &&
+              (this.grid[row][col] == 1 || this.grid[row][col] == 2)
+            ) {
+              this.gameOver = true;
+              this.winner = this.playerTurn;
+              break;
+            }
+          }
+          if (row - 3 >= 0 && col - 3 >= 0) {
+            if (
+              //check one diagonal
+              this.grid[row - 3][col - 3] == this.grid[row - 2][col - 2] &&
+              this.grid[row - 1][col - 1] == this.grid[row][col] &&
+              this.grid[row][col] == this.grid[row - 3][col - 3] &&
+              (this.grid[row][col] == 1 || this.grid[row][col] == 2)
+            ) {
+              this.gameOver = true;
+              this.winner = this.playerTurn;
+              break;
+            }
+          }
+          if (row - 3 >= 0 && col + 3 <= this.grid[row].length) {
+            if (
+              //check other diagonal
+              this.grid[row - 3][col + 3] == this.grid[row - 2][col + 2] &&
+              this.grid[row - 1][col + 1] == this.grid[row][col] &&
+              this.grid[row][col] == this.grid[row - 3][col + 3] &&
+              (this.grid[row][col] == 1 || this.grid[row][col] == 2)
+            ) {
+              this.gameOver = true;
+              this.winner = this.playerTurn;
+              break;
+            }
+          }
         }
       }
     }
