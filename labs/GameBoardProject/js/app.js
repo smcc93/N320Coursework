b69e7ce9550f6304c9cdd46595717d5357420194
Stewart <smccalle@iu.edu>
2019-11-05 21:03:27 -0500

    added win function

--- labs/GameBoardProject/js/app.js
+++ labs/GameBoardProject/js/app.js
@@ -5,7 +5,7 @@ var app = new Vue({
     gameOver: false,
     playerTurn: 1,
     winner: 0,
-    isActive: true,
+
     //play grids
     PlayerGrid: [
       [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
@@ -32,20 +32,22 @@ var app = new Vue({
       [1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
     ],
     //ships
-    playerShips: [
-      { type: "Detroyer", hits: 2, id: 1 },
-      { type: "Submarine", hits: 3, id: 2 },
-      { type: "Cruiser", hits: 3, id: 3 },
-      { type: "Battleship", hits: 4, id: 4 },
-      { type: "Aircraft Carrier", hits: 5, id: 5 }
-    ],
-    AIShips: [
-      { type: "Detroyer", hits: 2, id: 1 },
-      { type: "Submarine", hits: 3, id: 2 },
-      { type: "Cruiser", hits: 3, id: 3 },
-      { type: "Battleship", hits: 4, id: 4 },
-      { type: "Aircraft Carrier", hits: 5, id: 5 }
-    ]
+    playerShips: {
+      Detroyer: { hits: 2, id: 1 },
+      Submarine: { hits: 3, id: 2 },
+      Cruiser: { hits: 3, id: 3 },
+      Battleship: { hits: 4, id: 4 },
+      AircraftCarrier: { hits: 5, id: 5 },
+      Sunk: 0
+    },
+    AIShips: {
+      Detroyer: { hits: 2, id: 1 },
+      Submarine: { hits: 3, id: 2 },
+      Cruiser: { hits: 3, id: 3 },
+      Battleship: { hits: 4, id: 4 },
+      AircraftCarrier: { hits: 5, id: 5 },
+      Sunk: 0
+    }
   },
   methods: {
     selectCell: function(row, col) {
@@ -59,7 +61,10 @@ var app = new Vue({
             this.tempGrid[row][col].style.background = "#ffffff";
           } else {
             this.tempGrid[row][col].style.background = "#eb4034";
+            var loc = this.tempGrid[row][col];
+
             this.hit();
+            return loc;
           }
         }
       }
@@ -72,14 +77,82 @@ var app = new Vue({
 
       this.checkWin();
     },
-    hit: function() {
+    hit: function(loc) {
       if (playerTurn == 1) {
-      } else {
+        if (loc == AIShips.Detroyer.id) {
+          AIShips.Detroyer.hit--;
+          if (AIShips.Detroyer.hit == 0) {
+            AIShips.Sunk++;
+          }
+        }
+
+        if (loc == AIShips.Submarine.id) {
+          AIShips.Submarine.hit--;
+          if (AIShips.Submarine.hit == 0) {
+            AIShips.Sunk++;
+          }
+        }
+        if (loc == AIShips.Cruiser.id) {
+          AIShips.Cruiser.hit--;
+          if (AIShips.Cruiser.hit == 0) {
+            AIShips.Sunk++;
+          }
+        }
+        if (loc == AIShips.Battleship.id) {
+          AIShips.Battleship.hit--;
+          if (AIShips.Battleship.hit == 0) {
+            AIShips.Sunk++;
+          }
+          if (loc == AIShips.AircraftCarrier.id) {
+            AIShips.AircraftCarrier.hit--;
+            if (AIShips.AircraftCarrier.hit == 0) {
+              AIShips.Sunk++;
+            }
+          }
+        } else {
+          if (loc == playerShips.Detroyer.id) {
+            playerShips.Detroyer.hit--;
+            if (playerShips.Detroyer.hit == 0) {
+              playerShips.Sunk++;
+            }
+          }
+
+          if (loc == playerShips.Submarine.id) {
+            playerShips.Submarine.hit--;
+            if (playerShips.Submarine.hit == 0) {
+              playerShips.Sunk++;
+            }
+          }
+          if (loc == playerShips.Cruiser.id) {
+            playerShips.Cruiser.hit--;
+            if (playerShips.Cruiser.hit == 0) {
+              playerShips.Sunk++;
+            }
+          }
+          if (loc == playerShips.Battleship.id) {
+            playerShips.Battleship.hit--;
+            if (playerShips.Battleship.hit == 0) {
+              playerShips.Sunk++;
+            }
+          }
+          if (loc == playerShips.AircraftCarrier.id) {
+            playerShips.AircraftCarrier.hit--;
+            if (playerShips.AircraftCarrier.hit == 0) {
+              playerShips.Sunk++;
+            }
+          }
+        }
       }
     },
     checkWin: function() {
       if (playerTurn == 1) {
+        if (AIShips.Sunk == 5) {
+          winner == 1;
+        }
       } else {
+        if (playerShips.Sunk == 5) {
+          winner == 2;
+        }
       }
     }
   }
