70cf39d4e354c0873d1dd938aafac099c400485d
Stewart <smccalle@iu.edu>
2019-12-11 13:02:53 -0500

    audio lab from video with partial button name change

---
+++ labs/AudioLab/js/app.js
@@ -0,0 +1,42 @@
+var soundButtons = document.getElementById("soundButtons");
+
+var sounds = ["chimes_long", "click_clock_loop", "pop_10", "puff", "rustle_5"];
+
+var soundElements = [];
+
+//loop through the sounds and create audio tags
+sounds.forEach((soundUrl, idx) => {
+  //sounds
+  var newSound = new Audio("sounds/" + soundUrl + ".mp3");
+  soundElements.push(newSound);
+  //button
+  var newButton = document.createElement("button");
+  newButton.innerHTML = soundUrl;
+
+  //store the sound index
+  newButton.setAttribute("data-sound-id", idx);
+  //add it to the page
+  soundButtons.appendChild(newButton);
+
+  //listen for click
+  newButton.addEventListener("click", playSoundInArray);
+});
+
+function playSoundInArray() {
+  var soundIndex = Number(event.target.getAttribute("data-sound-id"));
+
+  //get sound from array
+  var selectedSound = soundElements[soundIndex];
+  selectedSound.play();
+}
+
+//get the audio tag
+// var myAudio = document.getElementById("myAudio");
+
+// function playAudio() {
+//   myAudio.play();
+// }
+
+// function stopMainAudio() {
+//   myAudio.pause();
+// }
